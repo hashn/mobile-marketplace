db23232fca0ed46a1bdcff9a0d98e06fcfd23d67
HeshanNayanajith <heshannayanajith@ieee.org>
2020-07-19 23:53:05 +0530

    get all listing

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { Listing_DAS } from 'src/app/services/listing.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+	selector: 'app-home',
+	templateUrl: './home.page.html',
+	styleUrls: ['./home.page.scss'],
 })
+
 export class HomePage implements OnInit {
 
-  constructor() { }
+	user;
+	data;
 
-  ngOnInit() {
-  }
+	constructor(private Router:Router, private Listings_DAS:Listing_DAS, private AuthenticationService:AuthenticationService) {
+		this.AuthenticationService.get_user().subscribe(user => {
+			this.user = user
+			this.Listings_DAS.get_listings().subscribe(Response => { this.data = Response })
+		})
+	}
+
+	ngOnInit() {
+	}
 
 }
